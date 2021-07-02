35a7c1da29fbea9bd33670d0d37f56f5ed2523ff
Tomas Rymon <tomas.rymon@gmail.com>
2021-07-02 23:53:39 +0200

    update with bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import copy
 import time
 from binance import Client, ThreadedWebsocketManager
-
 
 class BinanceClient:
     API_KEY = "lbhqUY1k95VIH2x0ahLLD78M40j4v9nUjJoYxaXCekpm80Xsenb9ujUO5IpryOOQ"
@@ -33,8 +33,13 @@ class Bot:
     frequency_of_purchase_in_seconds = 10
 
     income = 0
-    last_bought_position = None
-    bought_positions = []
+
+    own_positions = []
+    last_but_one_position = None
+    last_position = None
+    current_position = None
+    bought_current_position = None
+    selled_profitable_positions = []
 
     def __init__(self, currency_for_trading):
         self.currency_for_trading = currency_for_trading
@@ -43,56 +48,61 @@ class Bot:
         print(f"Bot started trading currency: {self.currency_for_trading}\n")
 
         while True:
-            time.sleep(self.frequency_of_purchase_in_seconds)
+            # time.sleep(self.frequency_of_purchase_in_seconds)
 
-            current_bought_position = self.get_currency_price()
+            self.set_up_positions()
 
-            selled_profitable_positions = []
+            if self.rules_met_to_buy():
+                self.buy_position(self.current_position)
 
             # Check income state
-            for position in self.bought_positions:
-                self.income += (current_bought_position - position)
+            for position in self.own_positions[:-1]:
+                self.income += (self.current_position - position)
 
-                # Remove selled profitable positions
-                if position < current_bought_position:
-                    self.bought_positions.remove(position)
-                    selled_profitable_positions.append(position)
+                # Sell profitable positions
+                if position < self.current_position:
+                    self.sell_position(position)
+
+            current_time = self.get_current_time()
 
 
-            self.last_bought_position = current_bought_position
-            self.bought_positions.append(current_bought_position)
-
-            # Get current time
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
-
-            print(f"{current_time} - New bought position - {current_bought_position}")
-            print(f"{len(self.bought_positions)} Own positions: {self.bought_positions}")
-            print(f"Selled positions: {selled_profitable_positions if selled_profitable_positions else 'none'}")
+            print(f"{current_time} - Current price - Bought: {'YES' if self.bought_current_position else 'NO'}")
+            print(f"{len(self.own_positions)} Own positions: {self.own_positions}")
+            print(f"Selled positions: {self.selled_profitable_positions if self.selled_profitable_positions else 'none'}")
             print(f"Income: {self.income}\n")
 
-    # def run_trading_session2(self):
-    #     print(f"Bot started trading currency: {self.currency_for_trading}")
-    #
-    #     while True:
-    #         time.sleep(self.frequency_of_purchase_in_seconds)
-    #
-    #         current_bought_position = self.get_currency_price()
-    #
-    #         if not self.bought_positions or (current_bought_position < self.last_bought_position):
-    #
-    #             self.last_bought_position = current_bought_position
-    #             self.bought_positions.append(current_bought_position)
-    #
-    #             t = time.localtime()
-    #             current_time = time.strftime("%H:%M:%S", t)
-    #
-    #             print(f"{current_time} - Bought position - {current_bought_position}")
-    #             print(f"Bought positions - {self.bought_positions}\n")
+
+    def rules_met_to_buy(self):
+        if not self.last_but_one_position or not self.last_position:
+            return False
+
+        if self.last_position < self.current_position:
+            return False
+
+        if (self.last_but_one_position > self.last_position) and (self.last_position > self.current_position):
+            return False
+
+        return True
+
+    def buy_position(self, position):
+        self.bought_current_position = position
+        self.own_positions.append(self.bought_current_position)
+
+    def sell_position(self, position):
+        self.own_positions.remove(position)
+        self.selled_profitable_positions.append(position)
+
+    def set_up_positions(self):
+        self.last_but_one_position = copy.deepcopy(self.last_position)
+        self.last_position =  copy.deepcopy(self.current_position)
+        self.current_position = self.get_currency_price()
 
     def get_currency_price(self):
-        binance_client = BinanceClient(self.currency_for_trading)
-        return binance_client.get_currency_price()
+        return BinanceClient(self.currency_for_trading).get_currency_price()
+
+    @staticmethod
+    def get_current_time():
+        return time.strftime("%H:%M:%S", time.localtime())
 
 bot = Bot("BTCUSDT")
 bot.run_trading_session1()
