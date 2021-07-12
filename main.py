bab4deb8005cb8b6559e55da3b39c6117712f98f
Tomas Rymon <tomas.rymon@directonlineservices.co.uk>
2021-07-12 15:24:40 +0200

    WIP

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import copy
 import time
 from binance import Client, ThreadedWebsocketManager
 
+
 class BinanceClient:
     API_KEY = "lbhqUY1k95VIH2x0ahLLD78M40j4v9nUjJoYxaXCekpm80Xsenb9ujUO5IpryOOQ"
     API_SECRET = "nxUn2pKY4p1P1Z84hgveal2ZAkmf6OR9M4K5Oo0J7gv6HJjFWLbCoG1o4JmSyX5q"
@@ -30,16 +31,16 @@ class BinanceClient:
 
 
 class Bot:
-    frequency_of_purchase_in_seconds = 10
+    frequency_of_purchase_in_seconds = 5
 
     income = 0
 
-    own_positions = []
     last_but_one_position = None
     last_position = None
     current_position = None
     bought_current_position = None
     selled_profitable_positions = []
+    own_positions = []
 
     def __init__(self, currency_for_trading):
         self.currency_for_trading = currency_for_trading
@@ -48,12 +49,15 @@ class Bot:
         print(f"Bot started trading currency: {self.currency_for_trading}\n")
 
         while True:
-            # time.sleep(self.frequency_of_purchase_in_seconds)
+            time.sleep(self.frequency_of_purchase_in_seconds)
 
+            # Set up current position, last but one position and last position.
             self.set_up_positions()
 
             if self.rules_met_to_buy():
                 self.buy_position(self.current_position)
+
+            selling_positions = []
 
             # Check income state
             for position in self.own_positions[:-1]:
@@ -62,15 +66,13 @@ class Bot:
                 # Sell profitable positions
                 if position < self.current_position:
                     self.sell_position(position)
+                    selling_positions.append(position)
 
-            current_time = self.get_current_time()
-
-
-            print(f"{current_time} - Current price - Bought: {'YES' if self.bought_current_position else 'NO'}")
+            print(f"{self.get_current_time()} - Current price: {self.current_position} - Bought: {'YES' if self.bought_current_position else 'NO'}")
+            print(f"Selling positions: {selling_positions if selling_positions else 'none'}")
+            print(f"All selled positions: {self.selled_profitable_positions if self.selled_profitable_positions else 'none'}")
             print(f"{len(self.own_positions)} Own positions: {self.own_positions}")
-            print(f"Selled positions: {self.selled_profitable_positions if self.selled_profitable_positions else 'none'}")
             print(f"Income: {self.income}\n")
-
 
     def rules_met_to_buy(self):
         if not self.last_but_one_position or not self.last_position:
@@ -86,7 +88,7 @@ class Bot:
 
     def buy_position(self, position):
         self.bought_current_position = position
-        self.own_positions.append(self.bought_current_position)
+        self.own_positions.append(position)
 
     def sell_position(self, position):
         self.own_positions.remove(position)
@@ -94,7 +96,7 @@ class Bot:
 
     def set_up_positions(self):
         self.last_but_one_position = copy.deepcopy(self.last_position)
-        self.last_position =  copy.deepcopy(self.current_position)
+        self.last_position = copy.deepcopy(self.current_position)
         self.current_position = self.get_currency_price()
 
     def get_currency_price(self):
@@ -103,6 +105,7 @@ class Bot:
     @staticmethod
     def get_current_time():
         return time.strftime("%H:%M:%S", time.localtime())
+
 
 bot = Bot("BTCUSDT")
 bot.run_trading_session1()
